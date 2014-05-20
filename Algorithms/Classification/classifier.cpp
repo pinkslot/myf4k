496f3112e0a019830480149d2e3dcce3e1671eab
pinkslot <pinkslot@mail.ru>
2014-05-20 20:45:00 +1100

    add oneClass SVM

--- Algorithms/Classification/classifier.cpp
+++ Algorithms/Classification/classifier.cpp
@@ -1,42 +1,35 @@
 #include "classifier.h"
-#include "opencv_utils.h"
-#include <log.h>
 #include <vector>
 #include <math.h>
 #include <complex>
-#include <iostream>
-#include <list>
-#include <array>
-#include <highgui.h>
-#include <svm_utils>
 
+#include <list>
+#include <highgui.h>
+#include <log.h>
 #include <histogram.h>
+#include <iostream>
 
 namespace alg
 {
-	Classifier::Classifier(filling_type ftype, string path) 
+	vector<svm_node> Classifier::extractAIM(Mat img)
 	{
-		
-	}
-
-	void extractAIM(Mat img, array<long double>(6) &ans)
-	{
-		int img[][3] = {1, 0 ,0 , 1 ,1 , 0};
+		if (img.channels()==4 || img.channels()==3)
+			cvtColor(img, img, CV_BGR2GRAY);			
+		threshold(img, img, 100, 255, 0);	
 		compd mean;
 		list<compd> points;
 		double n = 0;
-		for (int i = 0; i < 2; i++)
-			for (int j = 0; j < 3; j++)
-				if (img[i][j] == 1)
+		for (int i = 0; i != img.rows; i++)
+			for (int j = 0; j !=  img.cols; j++)
+				if (img.at<uchar>(i,j) == 255)
 				{
 					n++;
-					compd p = compd(i,j);
+					compd p = compd(i+1,j+1);
 					mean += p, points.push_back(p);
 				}
 		mean /= n;
 		compd conjmean = conj(mean);
-
-		array<compd,5> comp_mom;
+		vector<compd> comp_mom(5);
 		for (list<compd>::iterator pp = points.begin(); pp != points.end(); pp++)
 		{
 			compd c1 = *pp - mean, c2 = conj(*pp) - conjmean;
@@ -52,12 +45,19 @@ namespace alg
 		comp_mom[2] /= pow(n, 2.5);
 		comp_mom[3] /= pow(n, 2.5);
 		comp_mom[4] /= pow(n, 2.5);
-		ans[0] = real(comp_mom[0]);
-		ans[1] = real(compd(1e3,0)*comp_mom[2]*comp_mom[3]);
-		ans[2] = 1e4 * real(comp_mom[1]*comp_mom[3]*comp_mom[3]);
-		ans[3] = 1e4 * imag(comp_mom[1]*comp_mom[3]*comp_mom[3]);
-		ans[4] = 1e6 * real(comp_mom[4]*comp_mom[3]*comp_mom[3]*comp_mom[3]);
-		ans[5] = 1e6 * imag(comp_mom[4]*comp_mom[3]*comp_mom[3]*comp_mom[3]);
+
+		vector<svm_node> ans(7);	
+		ans[0].value = real(comp_mom[0]);
+		ans[1].value = real(compd(1e3,0)*comp_mom[2]*comp_mom[3]);
+		ans[2].value = 1e4 * real(comp_mom[1]*comp_mom[3]*comp_mom[3]);
+		ans[3].value = 1e4 * imag(comp_mom[1]*comp_mom[3]*comp_mom[3]);
+		ans[4].value = 1e6 * real(comp_mom[4]*comp_mom[3]*comp_mom[3]*comp_mom[3]);
+		ans[5].value = 1e6 * imag(comp_mom[4]*comp_mom[3]*comp_mom[3]*comp_mom[3]);
+		for (int i = 0; i < 6; i++)
+			ans[i].index = i+1;
+		ans[6].index = -1;
+		ans[6].value = -1;
+		return ans;
 	}		
 	// void Classifier::extractHist(const Mat& img)
 	// {
@@ -77,4 +77,95 @@ namespace alg
 	// 	features.push_back(c_moments[2]/pow(c_moments[0], 2));		// kurt
 
 	// }
+	bool Classifier::predict(const cvb::CvBlob &blob)
+	{
+		Mat mask = blobToMat(blob);
+		return predict(mask);
+	}
+
+	bool Classifier::predict(const Mat &mask)
+	{
+		// return anytime true if not init 
+		if (!model) return true;
+
+		vector<svm_node> feature = extractAIM(mask);
+		for (unsigned i = 0; i < feature.size(); i++)
+			feature[i].value = (feature[i].value-mean[i]) / dev[i];
+
+		// for (unsigned i = 0; i < feature.size(); i++)
+		// 	cout << feature[i].value << ' ' ;
+		// cout << endl;
+		double res = svm_predict(model, feature.data());
+		return res > 0;
+	}
+
+
+	void Classifier::trainModel(string path)
+	{
+		if ( !boost::filesystem::exists(path) ) 
+			throw MyException("Not such directory");
+		boost::filesystem::directory_iterator end; // default construction yields past-the-end
+
+		// vector pfeatures for store vector of each features, vector features - for store poiter to it's data
+		vector<svm_node *> features;
+		vector< vector<svm_node> > pfeatures;
+
+		for (boost::filesystem::directory_iterator itr(path); itr != end; itr++)
+		{
+			string fname = itr->path().filename().string();
+			// each files pair should have such name: <fname>src.bmp, <fname>msk.bmp
+			int suff_idx = fname.size()-7;
+			if (fname.size() < 7) 
+				continue;
+			string full_sec_fname =  path + fname.substr(0, suff_idx) + "msk.bmp";
+			Mat msk;
+			if (fname.substr(suff_idx) == string("src.bmp") && boost::filesystem::exists(full_sec_fname))
+				{
+					/*Log::info(0)*/cout <<  "train classifier on: " << full_sec_fname << endl;
+					msk = imread(full_sec_fname, CV_LOAD_IMAGE_GRAYSCALE);
+					if (!msk.data) 
+						throw MyException("Cannot read file" + full_sec_fname);  	 					
+					// prepare mask to extract features 
+
+  				pfeatures.push_back(extractAIM(msk));			
+  			}
+		}
+		unsigned feature_count = pfeatures.size(), feature_size = pfeatures[0].size();
+		mean = vector<double>(feature_size, 0), dev = vector<double>(feature_size, 0);
+		for (unsigned i  = 0; i < feature_count; i++)
+		{
+			features.push_back(pfeatures[i].data());
+			for (unsigned j = 0; j < feature_size; j++)
+				mean[j] += pfeatures[i][j].value, dev[j] += pfeatures[i][j].value * pfeatures[i][j].value;
+		}
+		for (unsigned j = 0; j < feature_size; j++)
+		{
+			mean[j] /= feature_count;
+			dev[j] = sqrt(dev[j]/feature_count - mean[j]*mean[j]);
+			cout << mean[j] << " " << dev[j] << endl;
+		}		
+
+		// normalize
+		for (unsigned i  = 0; i < feature_count; i++)
+			for (unsigned j = 0; j < feature_size; j++)
+					pfeatures[i][j].value = (pfeatures[i][j].value-mean[j]) / dev[j];
+
+		vector<double> label(features.size(), 1);			
+		svm_problem prob = {features.size(), label.data(), features.data()};			
+		svm_parameter param = {ONE_CLASS, RBF};
+		param.gamma = 0.4;
+		param.nu =  0.002;			
+		param.eps = 1e-6;
+		model = svm_train(&prob, &param);
+		string model_fname = path + "model.svm";
+		/*Log::info(0) */cout <<  "save model as " << model_fname << endl;
+		svm_save_model(model_fname.c_str(), model);
+	}		
+
+	void Classifier::loadModel(string path)
+	{
+		SVMUtils ut;
+		ut.load(path);
+		model = ut.svm;
+	}		
 }
--- Algorithms/Classification/classifier.h
+++ Algorithms/Classification/classifier.h
@@ -1,40 +1,51 @@
 #ifndef CLASSIFIER_H
 #define CLASSIFIER_H
 
-#include <iostream>
-#include <log.h>
 #include "../algorithm.h"
 #include <string>
 #include <svm.h>
+#include <opencv_utils.h>
+#include <svm_utils.h>
+#include <boost/filesystem.hpp>
+#include <my_exception.h>
+#include <cvblob.h>
 
 using namespace cv;
 using namespace std;
-typedef complex<long double> compd;
+typedef complex<double> compd;
 
 namespace alg
 {
-	enum filling_type {FILL_FROM_FILE, FILL_FROM_IMAGE, FILL_FROM_DB}
-	
 	class Classifier : public alg::Algorithm
 	{
 	private:
-		void extractAIM(const Mat &mask);
-		smv_model model;
-
+		vector<svm_node> extractAIM(Mat mask);
+		svm_model *model;
+		// save mean and dev of train set to normalize features 
+		vector<double> mean, dev;	
 	public:
 
 		//Methods	
-		Classifier(filling_type ftype, string path = 0);
+		Classifier(): model(0)
+		{
+		}
+		void trainModel(string path);
+		void loadModel(string path);
+		bool predict(const cvb::CvBlob &blob);
+		bool predict(const Mat &mask);
+
 		// Implement algorithm information
 		inline string name() const { return "classifier"; }
 		inline string type() const { return "classification"; }		
-		inline string description() const { return "One class SVM classifier"; }
+		inline string description() const { return "One class SVM classifier, \
+													this classifier require only positive \
+													example for train"; }
 		inline string version() const { return "1.0"; }
 		inline int executionTime() const { return 0; }
+		inline string descriptionExecutionTime() const { return "TODO"; }
 		inline int ram() const { return 0; }
 		inline string input() const { return "cvb::CvBlobs, cv::Mat"; }
 		inline string output() const { return "bool"; }		
 	};
-
 }
 #endif
